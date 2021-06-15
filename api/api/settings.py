4bbd003724e241d36ed595aabfe2eba04217aaa9
Hayagreev Varadarajan <hayagreev.chintu1@gmail.com>
2021-06-15 17:53:31 +0530

    edited settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MODELS = os.path.join(BASE_DIR, 'regressor/models')
+
+django_heroku.settings(locals())
