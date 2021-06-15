4008b2f066442fbba7c087b87e8580108489294d
Hayagreev Varadarajan <hayagreev.chintu1@gmail.com>
2021-06-15 18:03:07 +0530

    added allowed host

--- api/api/settings.py
+++ api/api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vh0o+_%2k21n4ni22lf72x(#jj4!oof7-j8wswnn84*!5n!6qn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
